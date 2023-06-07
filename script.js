9a78690e66a0ffd515df8d9a911e10109920e255
Janu Kumar Singh <119485859+jks6404@users.noreply.github.com>
2023-06-07 16:06:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
     //Write your code here
-
+   let words = document.getElementsByTagName("strong");
+   for(let i = 0; i < words.length; i++){
+    words[i].style.color = "green";
+   };
 
 }
 
 
 function return_normal() {
     //Write your code here
-
-    
+    let words = document.getElementsByTagName("strong");
+    for(let i = 0; i < words.length; i++){
+     words[i].style.color = "black";
+    };
 }
